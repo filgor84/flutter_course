4dd97c557459724394aea41f4e5202d6534434da
filgor84 <filgor84@gmail.com>
2020-03-14 10:57:22 +0100

    make MyApp stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,51 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print("Question Answered!");
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'How old are you?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello"),
+          title: Text("Quiz App"),
         ),
         body: Column(
           children: <Widget>[
-            Text("Default"),
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text("Red"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Green"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Blue"),
+              onPressed: answerQuestion,
+            ),
           ],
         ),
       ),
